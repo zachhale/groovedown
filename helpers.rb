b1994fca60e0d75ef3d8fde50d9d02256a06d2aa
Mel Gray <melgray@gmail.com>
2010-01-06 17:53:03 -0800

    External everything bruva

--- helpers.rb
+++ helpers.rb
@@ -3,7 +3,7 @@ module Groovedown
     # TODO: escape params
     def link_to(name, url, params={})
       url << "?#{params.map{|k,v|"#{k}=#{v}"}.join("&")}" unless params.empty?
-      %Q(<a href="#{url}">#{name}</a>)
+      %Q(<a href="#{url}" rel="external">#{name}</a>)
     end
   end
 end
