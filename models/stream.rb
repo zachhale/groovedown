512161b40d5c9eabe7149eb8707412f13ec3297e
Gun.io Whitespace Robot <contact@gun.io>
2011-10-27 02:40:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- models/stream.rb
+++ models/stream.rb
@@ -1,12 +1,12 @@
 class Stream
   attr_reader :song_id, :server, :key
-  
+
   def initialize(song_id)
     @song_id = song_id
 		request = { "header" => { "clientRevision" => "20091209.02", "client" => "gslite"}, "parameters" => { "songID" => @song_id.to_i, "prefetch" => false }, "method" => "getStreamKeyFromSongID" }
 
-    response = RestClient.post("http://cowbell.grooveshark.com/more.php?getStreamKeyFromSongID", 
-                               request.to_json, 
+    response = RestClient.post("http://cowbell.grooveshark.com/more.php?getStreamKeyFromSongID",
+                               request.to_json,
                                :content_type => "application/json")
 
 		@stream_result = JSON.parse(response)["result"]["result"]
